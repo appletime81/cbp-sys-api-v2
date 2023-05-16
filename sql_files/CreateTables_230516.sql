0e4f6d626c99baa6a16347d4d3c400ffa06e4ae2
appletime81 <itrimsl8696@gmail.com>
2023-05-16 20:35:10 +0800

    Update CreateTables_230516.sql

--- sql_files/CreateTables_230516.sql
+++ sql_files/CreateTables_230516.sql
@@ -9,9 +9,9 @@ CREATE TABLE InvoiceWKMaster
     IssueDate      datetime       DEFAULT NULL,
     DueDate        datetime       DEFAULT NULL,
     PartyName      varchar(100)   DEFAULT NULL,
-    IsPro          tinyint(1)     DEFAULT NULL,
-    IsRecharge     tinyint(1)     DEFAULT NULL,
-    IsLiability    tinyint(1)     DEFAULT NULL,
+    IsPro          tinyint(1) DEFAULT NULL,
+    IsRecharge     tinyint(1) DEFAULT NULL,
+    IsLiability    tinyint(1) DEFAULT NULL,
     TotalAmount    decimal(12, 2) DEFAULT NULL,
     PaidAmount     decimal(12, 2) DEFAULT NULL,
     CreateDate     datetime       DEFAULT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE InvoiceMaster
     ContractType   varchar(20)  DEFAULT NULL,
     IssueDate      datetime     DEFAULT NULL,
     DueDate        datetime     DEFAULT NULL,
-    IsPro          tinyint(1)   DEFAULT NULL,
+    IsPro          tinyint(1) DEFAULT NULL,
     Status         varchar(20)  DEFAULT NULL,
     PRIMARY KEY (InvMasterID)
 );
@@ -85,7 +85,7 @@ CREATE TABLE BillMaster
     FeeAmountSum      decimal(12, 2) DEFAULT NULL,
     ReceivedAmountSum decimal(12, 2) DEFAULT NULL,
     BankFees          decimal(12, 2) DEFAULT NULL,
-    IsPro             tinyint(1)     DEFAULT NULL,
+    IsPro             tinyint(1) DEFAULT NULL,
     Status            varchar(20)    DEFAULT NULL,
     URI               varchar(128),
     PRIMARY KEY (BillMasterID)
@@ -461,3 +461,86 @@ CREATE TABLE Corporates
     BranchAddress  varchar(512) DEFAULT NULL,
     PRIMARY KEY (CorpID)
 );
+
+/* Suppliers */
+INSERT INTO cbp_db_test_v2.Suppliers (SupplierID, SubmarineCable, WorkTitle, SupplierName, CompanyName, BankAcctName,
+                                      BankAcctNo, SavingAcctNo, SWIFTCode, IBAN, ACHNo, WireRouting, BankName, Branch,
+                                      BankAddress)
+VALUES (1, 'SJC2', 'Construction', 'NEC', 'NEC Corporation, Submarine Network Division', 'NEC Corporation', '',
+        '258008', 'SMBCJPJT', '', '', '', 'Sumitomo Mitsui Banking Corporation, Tokyo Main Office (211)', '',
+        '3-2, Marunouchi 1-chome, Chiyoda-ku, Tokyo, Japan NEC Corporation');
+
+/* Parties */
+INSERT INTO cbp_db_test_v2.Parties (PartyID, SubmarineCable, WorkTitle, PartyCode, PartyName, CompanyName, Address,
+                                    Contact, Email, Tel, BankAcctName, BankAcctNo, SavingAcctNo, SWIFTCode, IBAN, ACHNo,
+                                    WireRouting, BankName, Branch, BankAddress)
+VALUES (1, 'SJC2', 'Construction', 'CI', 'CHT', 'Chunghwa Telecom Co., Ltd. Network Technology Group',
+        '31, Aikuo East Road, Taipei, Taiwan 106', '黃宏杰', 'hj-hwang@cht.com.tw', '02-28054246', '', '', '', '', '',
+        '', '', '', '', '');
+INSERT INTO cbp_db_test_v2.Parties (PartyID, SubmarineCable, WorkTitle, PartyCode, PartyName, CompanyName, Address,
+                                    Contact, Email, Tel, BankAcctName, BankAcctNo, SavingAcctNo, SWIFTCode, IBAN, ACHNo,
+                                    WireRouting, BankName, Branch, BankAddress)
+VALUES (2, 'SJC2', 'Construction', 'CM', 'CM',
+        'China Mobile International Limited Transmission Engineering Manager | Planning & Development',
+        'Level 30, Tower 1, Kowloon Commerce Centre, No.51 Kwai Cheong Road, Kwai Chung, New Territories, Hong Kong',
+        'Anthony Wong (黄明华 WONG Ming Wah)', 'anthonywong@cmi.chinamobile.com', '+852 67656790', '', '', '', '', '',
+        '', '', '', '', '');
+INSERT INTO cbp_db_test_v2.Parties (PartyID, SubmarineCable, WorkTitle, PartyCode, PartyName, CompanyName, Address,
+                                    Contact, Email, Tel, BankAcctName, BankAcctNo, SavingAcctNo, SWIFTCode, IBAN, ACHNo,
+                                    WireRouting, BankName, Branch, BankAddress)
+VALUES (3, 'SJC2', 'Construction', 'DH', 'DHT', 'Donghua Telecom Co. Ltd.',
+        'UNIT A, 7/F, TOWER A, BILLION CENTRE, NO.1 WANG KWONG ROAD, KOWLOON BAY, KOWLOON, HKSAR', 'Frankie',
+        'frankie@donghwatele.com', '+852-35862600', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO cbp_db_test_v2.Parties (PartyID, SubmarineCable, WorkTitle, PartyCode, PartyName, CompanyName, Address,
+                                    Contact, Email, Tel, BankAcctName, BankAcctNo, SavingAcctNo, SWIFTCode, IBAN, ACHNo,
+                                    WireRouting, BankName, Branch, BankAddress)
+VALUES (4, 'SJC2', 'Construction', 'EG', 'EDGE', 'Edge Network Services Limited',
+        '4 Grand Canal Square, Dublin 2, Ireland', 'Nicholas', 'nictanch@fb.com', '+85295384781', '', '', '', '', '',
+        '', '', '', '', '');
+INSERT INTO cbp_db_test_v2.Parties (PartyID, SubmarineCable, WorkTitle, PartyCode, PartyName, CompanyName, Address,
+                                    Contact, Email, Tel, BankAcctName, BankAcctNo, SavingAcctNo, SWIFTCode, IBAN, ACHNo,
+                                    WireRouting, BankName, Branch, BankAddress)
+VALUES (5, 'SJC2', 'Construction', 'KD', 'KDDI',
+        'KDDI Corporation Global Network Engineering and Operations Center (GNOC)',
+        'KDDI Bldg., 2-3-2, Nishishinjuku, Shinjuku-ku, Tokyo 163-8003, Japan', 'Mr. Koki Takeshima',
+        'gnoc-cable@kddi.com', '+81-3-3347-6064', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO cbp_db_test_v2.Parties (PartyID, SubmarineCable, WorkTitle, PartyCode, PartyName, CompanyName, Address,
+                                    Contact, Email, Tel, BankAcctName, BankAcctNo, SavingAcctNo, SWIFTCode, IBAN, ACHNo,
+                                    WireRouting, BankName, Branch, BankAddress)
+VALUES (6, 'SJC2', 'Construction', 'ST', 'Singtel', 'Singapore Telecommunications Limited',
+        'COM3, #06-01, 31C Exeter Road Singapore 239734', 'Yue Meng Fai', 'mengfai@singtel.com', '+65 68872921', '', '',
+        '', '', '', '', '', '', '', '');
+INSERT INTO cbp_db_test_v2.Parties (PartyID, SubmarineCable, WorkTitle, PartyCode, PartyName, CompanyName, Address,
+                                    Contact, Email, Tel, BankAcctName, BankAcctNo, SavingAcctNo, SWIFTCode, IBAN, ACHNo,
+                                    WireRouting, BankName, Branch, BankAddress)
+VALUES (7, 'SJC2', 'Construction', 'SK', 'SKB', 'SK Broadband Co., Ltd. (SKB)',
+        '8F, SK Namsan Green Bldg., 24, Toegye-ro, Jung-gu, Seoul 04637, Korea', 'SUN JIN KUK (Chris)',
+        'chris.sun@sk.com', '+82-10-3702-0461', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO cbp_db_test_v2.Parties (PartyID, SubmarineCable, WorkTitle, PartyCode, PartyName, CompanyName, Address,
+                                    Contact, Email, Tel, BankAcctName, BankAcctNo, SavingAcctNo, SWIFTCode, IBAN, ACHNo,
+                                    WireRouting, BankName, Branch, BankAddress)
+VALUES (8, 'SJC2', 'Construction', 'TE', 'Telin',
+        'PT. Telekomunikasi Indonesia International (Telin) Digital & Service Planning and Development',
+        'Telkom Landmark Tower (TLT), Tower 2, 16thand 17th Floor,  Jalan Jendral Gatot Subroto, Kavling 52,Jakarta Selatan, Indonesia 12710',
+        'Mr. Edward Natanael', 'edward.natanael@telin.net', '+62 21 2995 2300', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO cbp_db_test_v2.Parties (PartyID, SubmarineCable, WorkTitle, PartyCode, PartyName, CompanyName, Address,
+                                    Contact, Email, Tel, BankAcctName, BankAcctNo, SavingAcctNo, SWIFTCode, IBAN, ACHNo,
+                                    WireRouting, BankName, Branch, BankAddress)
+VALUES (9, 'SJC2', 'Construction', 'TC', 'TICC',
+        'True Internet Corporation Co., Ltd (TICC) International Gateway Business',
+        '18, True Tower, Ratchadapisek Road, Huai Khwang, Bangkok 10310, Thailand', 'Wachira Parathum',
+        'wachira_par@trueintergateway.com', '+669 5651 5394', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO cbp_db_test_v2.Parties (PartyID, SubmarineCable, WorkTitle, PartyCode, PartyName, CompanyName, Address,
+                                    Contact, Email, Tel, BankAcctName, BankAcctNo, SavingAcctNo, SWIFTCode, IBAN, ACHNo,
+                                    WireRouting, BankName, Branch, BankAddress)
+VALUES (10, 'SJC2', 'Construction', 'VN', 'VNPT', 'VNPT NET Corporation (VNPT-NET)',
+        'Level 10, VNPT Net Building International Network Development Division 30 Pham Hung Street, My Dinh 1 Ward, Nam Tu Liem District, Hanoi, Vietnam',
+        'Minh Hien', 'nguyenthiminhhien@vnpt.vn', '+84 912312598', '', '', '', '', '', '', '', '', '', '');
+
+/* Corporates */
+INSERT INTO cbp_db_test_v2.Corporates (CorpID, SubmarineCable, WorkTitle, Address, CreateDate, BankAcctName, BankAcctNo,
+                                       SavingAcctNo, SWIFTCode, IBAN, ACHNo, WireRouting, BankName, Branch,
+                                       BranchAddress)
+VALUES (1, 'SJC2', 'Upgrade', '31 Aikuo E. Rd., Taipei, Taiwan, 106', '2023-04-14 03:36:23',
+        'SJC2 Central Billing Party of Chunghwa Telecom (International Business Group)', '054007501968', '',
+        'BKTWTWTP054', '', '', '', 'Bank of Taiwan, Hsinyi Branch', '', '88, Sec. 2, Sinyi Road, Taipei');
