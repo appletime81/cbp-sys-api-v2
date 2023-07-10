8f276c7b18d5b30a1367f3c8120ddc9ca9e4e607
appletime81 <itrimsl8696@gmail.com>
2023-07-10 16:35:07 +0800

    Update app.py

--- service/GlobalSearch/app.py
+++ service/GlobalSearch/app.py
@@ -43,9 +43,13 @@ async def searchBillMasterByInvoiceWKMaster(
     requestDictData = await request.json()
 
     crudInvoiceWKMaster = CRUD(db, InvoiceWKMasterDBModel)
+    crudInvoiceWKDetail = CRUD(db, InvoiceWKDetailDBModel)
+    crudBillDetail = CRUD(db, BillDetailDBModel)
+    crudBillMaster = CRUD(db, BillMasterDBModel)
 
     BillMilestone = requestDictData.pop("BillMilestone", None)
     Status = requestDictData.pop("Status", None)
+    InvoiceNo = requestDictData.pop("InvoiceNo", None)
     startDueDateString = requestDictData.pop("startDueDate", None)
     endDueDateString = requestDictData.pop("endDueDate", None)
     startDueDate = (
@@ -78,3 +82,64 @@ async def searchBillMasterByInvoiceWKMaster(
         InvoiceWKMasterData.WKMasterID
         for InvoiceWKMasterData in InvoiceWKMasterDataList
     ]
+
+    if BillMilestone:
+        InvoiceWKDetailDataList = crudInvoiceWKDetail.get_value_if_in_a_list(
+            InvoiceWKDetailDBModel.WKMasterID, WKMasterIDList
+        )
+        WKMasterIDList = list(
+            set(
+                [
+                    InvoiceWKDetailData.WKMasterID
+                    for InvoiceWKDetailData in InvoiceWKDetailDataList
+                    if InvoiceWKDetailData.BillMilestone == BillMilestone
+                ]
+            )
+        )
+
+    InvoiceWKMasterDataList = list(
+        filter(lambda x: x.WKMasterID in WKMasterIDList, InvoiceWKMasterDataList)
+    )
+
+    # =============================================
+    # 抓取 BillDetail(by WKMasterID)
+    # =============================================
+    BillDetailDataList = crudBillDetail.get_value_if_in_a_list(
+        BillDetailDBModel.WKMasterID, WKMasterIDList
+    )
+
+    # =============================================
+    # 組成結果
+    # =============================================
+    getResult = []
+    for InvoiceWKMasterData in InvoiceWKMasterDataList:
+        tempBillDetailDataList = list(
+            filter(
+                lambda x: x.WKMasterID == InvoiceWKMasterData.WKMasterID,
+                BillDetailDataList,
+            )
+        )
+        tempBillMasterIDList = list(
+            set(
+                [
+                    tempBillDetailData.BillMasterID
+                    for tempBillDetailData in tempBillDetailDataList
+                ]
+            )
+        )
+        getResult.append(
+            {
+                "InvoiceWKMaster": InvoiceWKMasterData,
+                "BillMaster": crudBillMaster.get_value_if_in_a_list(
+                    BillMasterDBModel.BillMasterID, tempBillMasterIDList
+                ),
+            }
+        )
+
+    if InvoiceNo:
+        getResult = [
+            result
+            for result in getResult
+            if InvoiceNo in result["InvoiceWKMaster"].InvoiceNo
+        ]
+    return getResult
